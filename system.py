54dd2f608a8868dd64be9ae263c5d8bd400ec88f
Kmarz23 <108611189+Kmarz23@users.noreply.github.com>
2023-09-03 10:53:48 +0200

    Update system.py

--- system.py
+++ system.py
@@ -1,6 +1,9 @@
 import datetime
+import socket
+import webbrowser
 import requests
-import shutil
+import json
+import re
 import os
 import time
 from cryptography.fernet import Fernet
@@ -16,15 +19,61 @@ if not os.path.exists("Programy"):
 
 # Inicjalizacja colorama
 init(autoreset=True)
+current_version = '0.0.1'  # Aktualna wersja Twojej aplikacji
 
 # Reszta kodu (jak wcześniej)
 
+def is_internet_available():
+    try:
+        # Próbujemy nawiązać połączenie z serwerem Google na porcie 80 (HTTP).
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
+
+def check_for_update():
+    if is_internet_available():
+        repo_owner = 'Kmarz23'
+        repo_name = 'PyCrafter'
+        
+        response = requests.get(f'https://api.github.com/repos/{repo_owner}/{repo_name}/releases')
+
+        if response.status_code == 200:
+            releases = json.loads(response.text)
+            if releases:
+                latest_release = releases[0]  # Zakładamy, że najnowszy release znajduje się na początku listy
+                latest_version = latest_release.get('tag_name')
+                if latest_version:
+                    if latest_version > current_version:
+                        print(f"Dostępna jest nowa wersja systemu: {latest_version}.")
+                        t_n = input("Naciśnij dowolny klawisz aby przejść do strony: ")
+                        url = "https://github.com/Kmarz23/Moj_OS/releases"  # Zastąp to adresem URL, na który chcesz przekierować użytkownika
+                        webbrowser.open(url)
+                    else:
+                        print("Masz już najnowszą wersję systemu.")
+                else:
+                    print("Nie znaleziono informacji o wersji w odpowiedzi z GitHuba.")
+            else:
+                print("Brak dostępnych wydań w repozytorium.")
+        else:
+            print("Błąd podczas pobierania danych z GitHuba.")
+    else:
+        print("Brak dostępu do internetu. Sprawdź swoje połączenie.")
+
+current_version = '1.0.0'  # Aktualna wersja Twojej aplikacji
+
+# Wywołaj funkcję check_for_update() w odpowiednim miejscu w aplikacji
+check_for_update()
+
 def print_loading_animation():
-    for i in range(1, 6):
-        loading_text = f"Ładowanie systemu... {i * 20}%"
+    total_steps = 100
+    for i in range(total_steps + 1):
+        loading_text = f"Ładowanie systemu... {i}%"
         colorized_loading_text = f"{Fore.RED}{loading_text}{Style.RESET_ALL}"
         print(colorized_loading_text, end="\r")
-        time.sleep(0.5)
+        time.sleep(0.03)
+    time.sleep(0.5)
     print(f"{Fore.GREEN}System załadowany!{Style.RESET_ALL}        ")
 
 def set_password():
@@ -61,17 +110,25 @@ def print_banner():
     os.system("clear")
     print_loading_animation()  # Wywołanie animacji ładowania
     now = datetime.datetime.now()
-    print(f"{now.strftime('%Y-%m-%d %H:%M:%S')}    ------------------")
-    print("                       |################|")
-    print(f"                       |###{Fore.YELLOW}888{Style.RESET_ALL}###{Fore.YELLOW}SSSS{Style.RESET_ALL}###|")
-    print(f"                       |##{Fore.YELLOW}8{Style.RESET_ALL}###{Fore.YELLOW}8{Style.RESET_ALL}#{Fore.YELLOW}S{Style.RESET_ALL}#######|")
-    print(f"                       |##{Fore.YELLOW}8{Style.RESET_ALL}###{Fore.YELLOW}8{Style.RESET_ALL}##{Fore.YELLOW}SSS{Style.RESET_ALL}####|")
-    print(f"                       |##{Fore.YELLOW}8{Style.RESET_ALL}###{Fore.YELLOW}8{Style.RESET_ALL}#####{Fore.YELLOW}S{Style.RESET_ALL}###|")
-    print(f"                       |###{Fore.YELLOW}888{Style.RESET_ALL}##{Fore.YELLOW}SSSS{Style.RESET_ALL}####|")
-    print("                       |################|")
-    print("                       ------------------")
+    print(f"{now.strftime('%Y-%m-%d %H:%M:%S')}    -------------------")
+    print("                       |#################|")
+    print(f"                       |#################|")
+    print(f"                       |#################|")
+    print(f"                       |####{Fore.YELLOW}PyCrafter{Style.RESET_ALL}####|")
+    print(f"                       |#################|")
+    print(f"                       |#################|")
+    print("                       |#################|")
+    print("                       -------------------")
+    print("                           By Kmarz")
 
 current_directory = "System"
+
+def dzienik_zmian():
+	print("Wersja: 1.0.0\n")
+	print("Wydano: 3.09.2023r.")
+	print("Zmiany:\n")
+	print("Stworzono system.\n")
+	input("Naciśnij enter aby powrucić do menu głównego ")
 
 def list_files():
     files = os.listdir('.')
@@ -140,7 +197,9 @@ def show_menu():
     print("1. Menedżer plików")
     print("2. Kalkulator")
     print("3. Gry")
-    print("4. Wyjdź\n")
+    print("4. Programy")
+    print("5. Dzienik zmian")
+    print("6. Wyjdź\n")
 
 def main():
     while True:
@@ -157,12 +216,37 @@ def main():
             elif choice == "3":
                 games_menu()
             elif choice == "4":
+            	programy_menu()
+            elif choice == "5":
+            	dzienik_zmian()
+            elif choice == "6":
                 print("Do widzenia!")
                 break
             else:
                 print("Nieprawidłowy wybór. Wybierz opcję 1, 2, 3, 4 lub 5.")
         else:
             print("Nieprawidłowe hasło. Dostęp zabroniony.")
+
+def programy_menu():
+    selected_file = 0
+    os.chdir("Programy")
+    while True:
+        print("\nProgramy")
+        files = [file for file in os.listdir('.') if file.endswith(".py")]
+        for idx, file in enumerate(files, start=1):
+            print(f"{idx}. {file}")
+
+        print(f"{len(files) + 1}. Wróć do głównego menu\n")  # Zmiana: korzystamy z len(files) zamiast idx
+        choice = input("Twój wybór: ")
+
+        if choice == str(len(files) + 1):  # Zmiana: porównujemy z len(files) + 1
+            os.chdir("..")
+            break
+        elif choice.isdigit() and 1 <= int(choice) <= len(files):
+            selected_file = files[int(choice) - 1]
+            os.system(f"python {selected_file}")
+        else:
+            print("Nieprawidłowy wybór")
 
 def games_menu():
     selected_file = 0
